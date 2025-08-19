59866eebd0eda5c8014762d2e374df83f629fe54
SD-W1972 <pernalongaesnobe@gmail.com>
2025-08-19 09:15:50 -0300

    feat: criando regras de negocio no UserService

---
+++ 0x05/hexagonal_architecture/src/domain/UserService.java
@@ -0,0 +1,29 @@
+package domain;
+
+import ports.UserRepository;
+
+import java.util.List;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public void createUser(String id, String name) {
+        //implemente o metodo
+    }
+
+
+    public User getUser(String id) {
+        //implemente o metodo
+    }
+
+
+    public List<User> listUsers() {
+        //implemente o metodo
+    }
+}
